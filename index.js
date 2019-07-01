82379d58d8552a77ca05901c70d1cdace0109da0
Joachim Schirrmacher <joachim.schirrmacher@deutschebahn.com>
2019-07-01 16:22:05 +0200

    Listen for POST requests as well

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 
 const app = express()
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
   console.log(JSON.stringify(req.query))
+  res.end()
+})
+
+app.post('/', (req, res) => {
+  console.log(JSON.stringify(req.body))
   res.end()
 })
 
