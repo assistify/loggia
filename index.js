23c34bb4f727083a311808f97e470db741feeb62
Joachim Schirrmacher <joachim.schirrmacher@deutschebahn.com>
2019-07-01 17:17:18 +0200

    Accept JSON encoded data as well

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser')
 
 const app = express()
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
   console.log(JSON.stringify(req.query))
